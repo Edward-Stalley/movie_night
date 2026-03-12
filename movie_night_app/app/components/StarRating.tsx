2eaa00adc8a6529a26f389f78c69a6ea362a2aad
Edward Stalley <stalley.e@impl.co.jp>
2026-03-12 10:40:38 +0900

    Changed File Names to PascalCase

---
+++ movie_night_app/app/components/StarRating.tsx
@@ -0,0 +1,41 @@
+import { StarRatingProps } from "@/lib/types/domain";
+import { useState } from "react";
+
+export default function StarRating({
+  rating,
+  max = 5,
+  onClick,
+  isEditing,
+}: StarRatingProps) {
+  const [hoverRating, setHoverRating] = useState<number | null>(null);
+  let displayRating = rating ?? 0;
+
+  if (isEditing && hoverRating !== null) {
+    displayRating = hoverRating;
+  }
+
+  return (
+    <div className="flex">
+      {Array.from({ length: max }).map((_, i) => {
+        const starValue = i + 1;
+        return (
+          <svg
+            key={i}
+            onClick={() => {
+              onClick?.(starValue);
+            }}
+            onMouseEnter={() => setHoverRating(starValue)}
+            onMouseLeave={() => setHoverRating(null)}
+            className={`w-5 h-5 ${
+              starValue <= displayRating ? "text-amber-400" : "text-gray-300"
+            } ${isEditing ? "cursor-pointer hover:text-amber-400" : ""}`}
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path d="M12 2l3 6 7 1-5 5 1 7-6-3-6 3 1-7-5-5 7-1z" />
+          </svg>
+        );
+      })}
+    </div>
+  );
+}
