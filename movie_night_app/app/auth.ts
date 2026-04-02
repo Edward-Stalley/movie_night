0b441fb3b317740f72009c9ccf0aee477ab9e71e
Edward Stalley <stalley.e@impl.co.jp>
2026-04-02 10:14:19 +0900

    Added troubleshoot logs

--- movie_night_app/app/auth.ts
+++ movie_night_app/app/auth.ts
@@ -4,6 +4,9 @@ import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
+console.log("AUTH_SECRET exists?", !!process.env.AUTH_SECRET);
+console.log("DB URL exists?", !!process.env.POSTGRES_DATABASE_PROD_URL);
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [
     GitHub,
