ff89fd33fe3091bb5fa853456f439644f2857d9a
Edward Stalley <stalley.e@impl.co.jp>
2026-04-13 16:58:16 +0900

    fixed unfinished conflict note

--- movie_night_app/lib/actions/deleteVotingSession.ts
+++ movie_night_app/lib/actions/deleteVotingSession.ts
@@ -1,11 +1,7 @@
 'use server';
 
 import { deleteVoteSession } from '@/lib/queries/vote';
-<<<<<<< Updated upstream
-// import { revalidatePath } from 'next/cache';
-=======
 import { revalidatePath } from 'next/cache';
->>>>>>> Stashed changes
 
 export async function deleteVoteSessionAction(id: number) {
   revalidatePath('/vote-session'); // adjust to your route
