b0a7d8c59d6a9c9ac8ac9c71371d455715b20cf7
Edward Stalley <stalley.e@impl.co.jp>
2026-04-14 12:59:40 +0900

    fixed detail for mobile

--- movie_night_app/app/components/movies/MovieListItem.tsx
+++ movie_night_app/app/components/movies/MovieListItem.tsx
@@ -12,26 +12,32 @@ export function MovieListItem({ movie }: MovieListItemProps) {
   const trailerId = getYouTubeId(movie.trailerUrl);
 
   return (
-    <div className="flex flex-col  gap-4 bg-base-300 m-2 p-2 rounded-2xl">
-      <div className="flex relative">
-        <MoviePoster
-          id={movie.id}
-          posterPath={movie.posterPath}
-          title={movie.title}
-          urlRoute="movies"
-          className="rounded-2xl"
-        />
-        <div className="w-full flex flex-col  bg-base-200 p-2  ">
-          <div className=" p-4 bg-base-200 rounded-2xl flex-2 ">{movie.overview}</div>
-          <div className="flex-1 w-64 rounded-2xl p-2">
-            <p className="font-bold text-1xl text-primary  border-b-2 p-2">Trailer</p>
+    <div className="flex flex-col gap-4 bg-base-300 m-2 rounded-2xl relative">
+      <div className="flex flex-col gap-2 pl-2 lg:flex-row md:flex-row">
+        <p className='font-extrabold text-lg'>{movie.title}</p>
+        <div className="flex ">
+          <MoviePoster
+            id={movie.id}
+            posterPath={movie.posterPath}
+            title={movie.title}
+            urlRoute="movies"
+            className="rounded-2xl"
+          />
+        </div>
+        <div className=" p-4 bg-base-200 rounded-2xl flex-2 ">{movie.overview}</div>
+        <div className="flex flex-col  bg-base-200 p-2 flex-1  ">
+          <div className="flex-1 rounded-2xl p-2">
+            <div className="border-b-2 p-2 flex justify-between">
+              <p className="font-bold text-1xl text-primary ">Trailer</p>
+              {movie.trailerUrl && <TrailerLinkButton trailerLink={movie.trailerUrl} />}
+            </div>
+
             {trailerId ? (
               <YouTubeEmbed videoid={trailerId} />
             ) : (
               <p className="p-2">No Trailer Available</p>
             )}
           </div>
-          {movie.trailerUrl && <TrailerLinkButton trailerLink={movie.trailerUrl} />}
         </div>
       </div>
     </div>
